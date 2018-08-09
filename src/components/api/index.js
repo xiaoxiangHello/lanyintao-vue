3c39be60be62c7f697e1f907d085e900f010ae4c
bingqinglj <303625635@qq.com>
2018-08-09 18:42:10 +0800

    api

---
+++ src/components/api/index.js
@@ -0,0 +1,8 @@
+var API_PREFIX = "//tousu.sina.cn/api/";
+
+module.exports = {
+	LAWS_FEED: API_PREFIX + "laws/feed",
+	INDEX_FEED: API_PREFIX + "index/feed",
+	PRAISE: API_PREFIX + "votes/add",
+	MY_COMPLAINTS: API_PREFIX + "user/complaints"
+};
