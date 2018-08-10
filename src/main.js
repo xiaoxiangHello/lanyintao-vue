22679c1ab2d9b414eeee0c362c86b0ebcc94180c
bingqinglj <303625635@qq.com>
2018-08-10 16:11:12 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ import 'element-ui/lib/theme-chalk/index.css';
 import store from './store'
 import axios from 'axios'
 import VueJsonp from 'vue-jsonp'
+import vueLazyImg from './js/lazy-img.js'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios;
 
 Vue.use(ElementUI);
 Vue.use(VueJsonp);
+Vue.use(vueLazyImg)
+
 new Vue({
     router,
     store,
